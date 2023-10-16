1b6443858622b2d0d5d4b8c063a5877dd1322409
phakoda <130813466+phakoda@users.noreply.github.com>
2023-10-15 20:02:39 -0400

    full movie stuff

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://httphandler.unsure-gallon0f.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2Q2OWRhODc3MTg4NTE4MDg5MGY4NDM2OGU5ZGNiYyIsInN1YiI6IjY1MmM3YjQ5MzU4ZGE3NWI1YzBiYmE0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.djHtxC4FBjKUlZPjg1ThDXCMSfuhF-tkreQ3kI8onWY"
+};

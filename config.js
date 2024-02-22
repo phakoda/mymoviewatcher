9156a083b9003c10317559b817346c389fa92ad6
phakoda <130813466+phakoda@users.noreply.github.com>
2024-02-22 18:11:41 -0500

    new movie-web updates

--- config.js
+++ config.js
@@ -1,5 +1,19 @@
 window.__CONFIG__ = {
-  // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://httphandler.unsure-gallon0f.workers.dev",
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2Q2OWRhODc3MTg4NTE4MDg5MGY4NDM2OGU5ZGNiYyIsInN1YiI6IjY1MmM3YjQ5MzU4ZGE3NWI1YzBiYmE0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.djHtxC4FBjKUlZPjg1ThDXCMSfuhF-tkreQ3kI8onWY"
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  VITE_CORS_PROXY_URL: "CHANGEME",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "CHANGEME",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
 };
